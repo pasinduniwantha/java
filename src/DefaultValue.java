e00fb88541aaa49c2b69887a7064042bb7dbc127
pasi <pasinduniwantha4@gmail.com>
2023-03-27 10:43:51 +0530

    java default value code

---
+++ src/DefaultValue.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class DefaultValue {
+    int x;
+    boolean y;
+    public static void main(String[] args) {
+        DefaultValue dv = new DefaultValue();
+        System.out.println(dv.x);
+        System.out.println(dv.y);
+    }
+
+}
